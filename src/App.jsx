eccc6655cface9ee2b422a40af018c4907e017f4
Boba? <bondanbanuaji@gmail.com>
2025-02-23 15:50:06 +0700

    add Navbar components

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
-import Hero from './components/hero'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero';
 import About from './components/About';
 
-
 const App = () => {
-  return (
-    <main className="relative min-h-screen w-screen overflow-x-hidden">
-      <Hero />
-      <About />
-    </main>
-  )
+    return (
+        <main className="relative w-screen min-h-screen overflow-x-hidden">
+            <Navbar />
+            <Hero />
+            <About />
+        </main>
+    )
 }
 
 export default App
